d9bdfe424619fc335e9c03f3d16495d25c2c45a6
jaregauri <37548452+jaregauri@users.noreply.github.com>
2020-08-26 22:26:21 +0530

    Update SVM Classifier.py

--- SVM Classifier.py
+++ SVM Classifier.py
@@ -13,6 +13,10 @@ df = pd.read_csv('Social_Network_Ads.csv')
 X = df.iloc[:, :-1].values
 y = df.iloc[:, -1].values
 
+#pandas profiling
+import pandas_profiling as pp
+a = pp.ProfileReport(df)
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
